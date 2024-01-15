2ede1b83bb8f2a6b15e6a31f2a0336696359f19c
Nooonchill <nooonchill@gmail.com>
2024-01-16 01:19:47 +0500

    005 output models

--- djangoProject/vacancies_stat/models.py
+++ djangoProject/vacancies_stat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField("Имя", max_length=64)
+    last_name = models.CharField("Фамилия", max_length=64)
+    age = models.IntegerField("Возраст")
+    job = models.CharField("Должность", max_length=64)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Человек"
+        verbose_name_plural = "Люди"
--- djangoProject/vacancies_stat/views.py
+++ djangoProject/vacancies_stat/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+from django.shortcuts import render
+from vacancies_stat.models import Person
+
 def index_page(request):
-    return render(request, 'index.html')
+    persons = Person.objects.all()
+    s = ""
+    for person in persons:
+        s += f"<h3>{person}: возраст - {person.age}, должность - {person.job.lower()}</h3>"
+    return render(request, 'index.html', context={"person_list": s})
