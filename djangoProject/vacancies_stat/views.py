c108df4d36bbbf5cd01a51bd91ae0c3ed3646113
Nooonchill <nooonchill@gmail.com>
2024-01-19 14:48:33 +0500

    011

--- djangoProject/vacancies_stat/views.py
+++ djangoProject/vacancies_stat/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import pandas as pd
 from django.shortcuts import render
 from vacancies_stat.models import Person
-from stat_create import create_plot
+from stat_former import create_plot
 
 def index_page(request):
 
