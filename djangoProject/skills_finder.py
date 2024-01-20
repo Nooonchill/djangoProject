cc40ff3a92a3ca67bdcbdb2108678d11dada2c9e
Nooonchill <nooonchill@gmail.com>
2024-01-20 19:19:32 +0500

    022 html css start

--- djangoProject/skills_finder.py
+++ djangoProject/skills_finder.py
@@ -3,7 +3,7 @@ import pandas as pd
 from collections import Counter
 
 conn = sqlite3.connect('db.sqlite3')
-df = pd.read_sql('SELECT name, key_skills, date FROM vacancies_stat_vacancy LIMIT 1236484', conn)
+df = pd.read_sql('SELECT name, key_skills, date FROM vacancies_stat_vacancy LIMIT 2236484', conn)
 df = df.assign(year=df['date'].str[:4])
 df = df.dropna()
 
@@ -12,7 +12,10 @@ df = df[['key_skills', 'year']]
 df = df.groupby(by='year').sum()
 dic = df['key_skills'].to_dict()
 print(df['key_skills'].to_dict())
+skills_dic = {}
 for i in dic.keys():
-    print(Counter(dic[i]))
-
+    a = sorted(Counter(dic[i]).items(), key=lambda item: item[1], reverse=True)[:20]
+    print(dict(a))
+    skills_dic[i] = dict(a)
+print(skills_dic)
 
